80d4f68c6747cd20022a28e3d543ed4cbda71e46
Eliza Weisman <eliza@elizas.website>
2024-02-05 11:51:01 -0800

    okay there we go

--- flake.nix
+++ flake.nix
@@ -3,103 +3,111 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, flake-utils, ... }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = nixpkgs.legacyPackages.${system};
-        gitRev = "be886696affec833859fea4b44a560539cd52519";
-        src = pkgs.fetchFromGitHub {
-          owner = "SasaKaranovic";
-          repo = "VU-Server";
-          rev = gitRev;
-          sha256 = "sha256-+CgGU25xM6/zse0ctaE3gxM7duUAKj9v0kJ1k5+zwrc=";
-        };
+  outputs = { self, nixpkgs, ... }:
+    let
+      forAllSystems = function:
+        nixpkgs.lib.genAttrs [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ]
+          (system: function nixpkgs.legacyPackages.${system});
 
-        python = pkgs.python3.withPackages
-          (pythonPkgs: with pythonPkgs; [
-            tornado
-            numpy
-            pillow
-            requests
-            pyyaml
-            ruamel-yaml
-            pyserial
-          ]);
+      rev = "be886696affec833859fea4b44a560539cd52519";
+      name = "VU-Server";
+      mkPackage = { pkgs }:
+        let
 
-        pythonPkg = pkgs.stdenv.mkDerivation
-          {
-            name = "VU-Server";
-            inherit src;
-            installPhase = ''
-              mkdir -p $out/bin
-              cp -r . $out/bin'';
-            propagatedBuildInputs = [ python ];
+          src = pkgs.fetchFromGitHub {
+            owner = "SasaKaranovic";
+            repo = name;
+            inherit rev;
+            sha256 = "sha256-+CgGU25xM6/zse0ctaE3gxM7duUAKj9v0kJ1k5+zwrc=";
           };
-        runScript = pkgs.writeShellApplication
-          {
-            name = "vu-server-runner";
+          python = pkgs.python3.withPackages
+            (pythonPkgs: with pythonPkgs; [
+              tornado
+              numpy
+              pillow
+              requests
+              pyyaml
+              ruamel-yaml
+              pyserial
+            ]);
 
-            runtimeInputs = [ pythonPkg python ];
+          serverPkg = pkgs.stdenv.mkDerivation
+            {
+              inherit src name;
+              installPhase = ''
+                mkdir -p $out/bin
+                cp -r . $out/bin'';
+              propagatedBuildInputs = [ python ];
+            };
+          runScript = pkgs.writeShellApplication
+            {
+              name = "vu-server-runner";
 
-            text = ''
-              set -x
-              cp --recursive \
-                --no-preserve=mode \
-                --t "$tmp"/ \
-                ${pythonPkg}/bin/*
-              ${python}/bin/python "$tmp"/server.py
-            '';
-          };
-        nixosModule = { config, lib, pkgs, ... }: with lib; let
-          cfg = config.services.vu-server;
-          pkg = self.${system}.packages.vu-server;
+              runtimeInputs = [ serverPkg python ];
+
+              text = ''
+                set -x
+                cp --recursive \
+                  --no-preserve=mode \
+                  --t "$tmp"/ \
+                  ${serverPkg}/bin/*
+                  python "$tmp"/server.py
+              '';
+            };
         in
         {
-          options.services.vu-server = {
-            enable = mkEnableOption "VU-Server systemd service";
-          };
-
-          config = mkIf cfg.enable {
-            wantedBy = [ "multi-user.target" ];
-            script = ''
-              set -x
-              cp --recursive \
-                --no-preserve=mode \
-                --t . / \
-                ${pkg}/bin/*
-              ${python}/bin/python "$tmp"/server.py
-            '';
-
-            serviceConfig = {
-              Restart = "on-failure";
-              DynamicUser = "yes";
-              RuntimeDirectory = "vu-server";
-              RuntimeDirectoryMode = "0755";
-              StateDirectory = "vu-server";
-              StateDirectoryMode = "0700";
-              CacheDirectory = "vu-server";
-              CacheDirectoryMode = "0750";
-            };
-          };
-
+          inherit python runScript;
+          vu-server = serverPkg;
         };
+
+      nixosModule = { config, lib, pkgs, ... }: with lib; let
+        cfg = config.services.vu-server;
+        pkg = mkPackage { inherit pkgs; };
       in
       {
-        packages =
-          {
-            default = self.packages.${system}.vu-server;
-            vu-server = pythonPkg;
-            runner = runScript;
-          };
-        nixosModules.default = nixosModule;
-        apps.default = {
-          type = "app";
-          program = "${self.packages.${system}.runner}/bin/vu-server-runner";
+        options.services.vu-server = {
+          enable = mkEnableOption "VU-Server systemd service";
         };
-      });
-}
 
+        config = mkIf cfg.enable {
+          wantedBy = [ "multi-user.target" ];
+          script = ''
+            set -x
+            cp --recursive \
+              --no-preserve=mode \
+              --t . / \
+              ${pkg.pkg}/bin/*
+            ${pkg.python}/bin/python "$tmp"/server.py
+          '';
+
+          serviceConfig = {
+            Restart = "on-failure";
+            DynamicUser = "yes";
+            RuntimeDirectory = "vu-server";
+            RuntimeDirectoryMode = "0755";
+            StateDirectory = "vu-server";
+            StateDirectoryMode = "0700";
+            CacheDirectory = "vu-server";
+            CacheDirectoryMode = "0750";
+          };
+        };
+      };
+    in
+    {
+      nixosModules.default = nixosModule;
+      packages = forAllSystems (pkgs:
+        let pkg = mkPackage { inherit pkgs; }; in {
+          default = pkg.vu-server;
+          vu-server = pkg.vu-server;
+          runner = pkg.runScript;
+        });
+      apps =
+        nixpkgs.lib.genAttrs [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ]
+          (system: {
+            default = { type = "app"; program = "${self.packages.${system}.runner}/bin/vu-server-runner"; };
+          });
+    };
+}
 

d3e9f8e27f9127b47af1ee5ecc0d89aec79450ac
Eliza Weisman <eliza@elizas.website>
2024-02-05 15:34:05 -0800

    blah

--- flake.nix
+++ flake.nix
@@ -131,7 +131,7 @@
               cd "$RUNTIME_DIRECTORY"
               cp --recursive \
                 --no-preserve=mode \
-                --update=oldest \
+                --update=older \
                 ${pkg.vu-server}/bin/* .
               ln --symbolic --force "$STATE_DIRECTORY"/vudials.db .
               ln --symbolic --force /etc/vu-server/config.yaml .
